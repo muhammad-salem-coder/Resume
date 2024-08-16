2608ae0870eebd91baf9e483a84cf3390c03c829
muhammad-salem-coder <muh.salem1403@gmail.com>
2024-08-16 12:54:41 +0400

    trying to add CDN links 2

--- dist/functions.js
+++ dist/functions.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
 import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm';
-import { GLTFLoader } from 'https://unpkg.com/three@0.126.0/examples/js/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/js/loaders/GLTFLoader.js';
 import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.11.0/dist/gsap.min.js';
 import anime from 'https://cdn.jsdelivr.net/npm/animejs@3.2.2/lib/anime.min.js';
 
