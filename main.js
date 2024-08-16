dee0f41a23e312da0f6bfb2882d0f7878ec8c691
muhammad-salem-coder <muh.salem1403@gmail.com>
2024-08-16 17:39:01 +0400

    Updates

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js';
 import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm';
-import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.11.0/dist/gsap.min.js';
-import { RGBELoader, RenderPass, EffectComposer, OutputPass } from 'https://cdn.jsdelivr.net/npm/three-addons@1.2.0/build/three-addons.min.js';
-import { OutlinePass } from 'https://cdn.jsdelivr.net/npm/three-outlinepass@1.2.0/OutlinePass.min.js';
-import { TransformControls } from 'https://cdn.jsdelivr.net/npm/threejs-transformcontrols@0.83.0/index.min.js';
-import { GUI } from 'https://cdn.jsdelivr.net/npm/lil-gui@0.19.2/dist/lil-gui.umd.min.js';
 import * as functions from './functions.js';
 // _______________________________________________________________ //
 
