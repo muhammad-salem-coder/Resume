41648924589b4e62cceb0102064938f2949531de
muhammad-salem-coder <muh.salem1403@gmail.com>
2024-08-16 10:23:15 +0400

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js';
 import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm';
 import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.11.0/dist/gsap.min.js';
 import { RGBELoader, RenderPass, EffectComposer, OutputPass } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/postprocessing/EffectComposer.js';
@@ -7,7 +7,6 @@ import { OutlinePass } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples
 import { TransformControls } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/controls/TransformControls.js';
 import { GUI } from 'https://cdn.jsdelivr.net/npm/lil-gui@0.19.2/dist/lil-gui.umd.min.js';
 import * as functions from './functions.js';
-import './main.css';
 // _______________________________________________________________ //
 
 const sizes = {
